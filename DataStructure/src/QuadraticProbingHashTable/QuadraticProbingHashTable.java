2ebc234c63aaca43344d71dda8c05e6c9c0968c5
Charles-Zhou <sysuzzh@sina.com>
2015-10-19 23:55:38 +0800

    version 1.2.2: add the Map.java

--- DataStructure/src/QuadraticProbingHashTable/QuadraticProbingHashTable.java
+++ DataStructure/src/QuadraticProbingHashTable/QuadraticProbingHashTable.java
@@ -20,6 +20,35 @@ public class QuadraticProbingHashTable<T> {
 		currentSize=0;
 	}
 	
+	public boolean contains(T x) {
+		int currentPos=findPos(x);
+		
+		return isActive(currentPos);
+	}
+	
+	public void insert(T x) {
+		int currentPos=findPos(x);
+		
+		if(isActive(currentPos))
+			return;
+		
+		array[currentPos] =new HashEntry<T>(x);
+		
+		if(++currentSize>array.length/2)
+			rehash();
+	}
+	
+	public void remove(T x) {
+		int currentPos=findPos(x);
+		
+		if(isActive(currentPos))
+			array[currentPos].isAcitve=false;
+	}
+	
+	public T getItem(int Pos) {
+		return array[Pos].element;
+	}
+	
 	private static class HashEntry<T> {
 		public T element;
 		public boolean isAcitve;
@@ -41,15 +70,41 @@ public class QuadraticProbingHashTable<T> {
 	private int currentSize;
 	
 	private void allocateArray(int arraySize) {
-		array=(HashEntry<T>[]) new Object[arraySize];
+		array=new HashEntry[arraySize];
 	}
 	
-	private boolean isActive(int currentPos) {
-		return array[currentPos].isAcitve==true;
+	public boolean isActive(int currentPos) {
+		return array[currentPos]!=null && array[currentPos].isAcitve;
 	}
 	
-	private int findPos(T x) {
+	public int findPos(T x) {
+		int currentPos=myhash(x);
+		int i=1;
 		
+		
+		while(array[currentPos]!=null && !array[currentPos].element.equals(x)) {
+			currentPos+=2*i-1;
+			
+			i++;
+			
+			if(currentPos>=array.length)
+				currentPos+=array.length;
+		}
+		
+		return currentPos;
+			
+	}
+	
+	private void rehash() {
+		HashEntry<T>[] old=array;
+		
+		allocateArray(2*array.length);
+		
+		currentSize=0;
+		
+		for(HashEntry<T> item:old)
+			if(item!=null && item.isAcitve)
+				insert(item.element);
 	}
 	
 	private int myhash(T x) {
